0145e0bc1d77e3ca68d159b9f8468aaf9c053b32
cmalinmayor <cmalinmayor@gmail.com>
2023-03-09 16:23:11 -0500

    Add explicit constructor and ctc events to TrackEvents

--- src/cell_tracking_metrics/track_errors/track_events.py
+++ src/cell_tracking_metrics/track_errors/track_events.py
@@ -1,10 +1,3 @@
-from dataclasses import dataclass
-from typing import Optional
-
-from .division_events import DivisionEvents
-
-
-@dataclass
 class TrackEvents:
     """A class to hold counts of tracking events or errors.
 
@@ -24,6 +17,13 @@ class TrackEvents:
             (present in the predicted graph but not matched). Defaults to None.
         fn_edges (int): The number of false negative edges
             (present in the gt graph but not matched). Defaults to None.
+        identity_switches (int): The number of adjacent edges in the ground truth
+            that are matched to non-consecutive edges in the prediction.
+            This will only be non-zero if ground truth nodes can be matched
+            to multiple predicted nodes.
+
+        nonsplit_vertices (int):
+        incorrect_semantics (int):
 
         division_counts(dict[int, DivisionEvents]): A mapping
             from a "frame buffer" (number of frames within which a division can
@@ -33,17 +33,34 @@ class TrackEvents:
             predicted within 1 frame of the gt division are considered correct.
     """
 
-    # nodes
-    fp_nodes: Optional[int] = None
-    fn_nodes: Optional[int] = None
-
-    # edges
-    fp_edges: Optional[int] = None
-    fn_edges: Optional[int] = None
-
-    division_counts: Optional[dict[int, DivisionEvents]] = None
+    def __init__(
+        self,
+        fp_nodes=None,
+        fn_nodes=None,
+        fp_edges=None,
+        fn_edges=None,
+        identity_switches=None,
+        nonsplit_vertices=None,
+        incorrect_semantics=None,
+        division_counts=None,
+    ):
+        self.fp_nodes = fp_nodes
+        self.fn_nodes = fn_nodes
+        self.fp_edges = fp_edges
+        self.fn_edges = fn_edges
+        self.identity_switches = identity_switches
+        self.nonsplit_vertices = nonsplit_vertices
+        self.incorrect_semantics = incorrect_semantics
+        self.division_counts = {}
+        if division_counts is not None:
+            self.division_counts[division_counts.frame_buffer] = division_counts
 
     def add_division_counts(self, frame_buffer, counts):
         if self.division_counts is None:
             self.division_counts = {}
         self.division_counts[frame_buffer] = counts
+
+    def get_division_counts(self, frame_buffer=0):
+        if frame_buffer not in self.division_counts:
+            return None
+        return self.division_counts[frame_buffer]
