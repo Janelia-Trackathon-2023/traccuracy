37c0c4cff40aea4e1cd5da685d310b9d08d07079
cmalinmayor <cmalinmayor@gmail.com>
2023-03-08 22:18:26 -0500

    Directly import main classes

--- src/cell_tracking_metrics/__init__.py
+++ src/cell_tracking_metrics/__init__.py
@@ -8,3 +8,6 @@ except PackageNotFoundError:
 
 __author__ = "Track Gals"
 __email__ = "draga.doncilapop1@monash.edu"
+
+from .tracking_data import TrackingData
+from .tracking_graph import TrackingGraph
