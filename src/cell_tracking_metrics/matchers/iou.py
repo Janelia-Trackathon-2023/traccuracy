abd608ea8c87c55b823b1bd64b9169d72bc149aa
msschwartz21 <msschwartz21@gmail.com>
2023-03-13 16:13:04 -0400

    Create IOUMatched class

--- src/cell_tracking_metrics/matchers/iou.py
+++ src/cell_tracking_metrics/matchers/iou.py
@@ -5,6 +5,8 @@ from cell_tracking_metrics.matchers.compute_overlap import (
 )
 from cell_tracking_metrics.tracking_data import TrackingData
 
+from .matched import Matched
+
 
 def _match_nodes(gt, res, threshold=1):
     """Identify overlapping objects according to IoU and a threshold for minimum overlap.
@@ -12,8 +14,8 @@ def _match_nodes(gt, res, threshold=1):
     QUESTION: Does this rely on sequential segmentation labels
 
     Args:
-        gt (np.ndarray): labeled frame (2D)
-        res (np.ndarray): labeled frame (2D)
+        gt (np.ndarray): labeled frame
+        res (np.ndarray): labeled frame
         threshold (optional, float): threshold value for IoU to count as same cell. Default 1.
             If segmentations are identical, 1 works well.
             For imperfect segmentations try 0.6-0.8 to get better matching
@@ -21,9 +23,6 @@ def _match_nodes(gt, res, threshold=1):
         gtcells (np arr): Array of overlapping ids in the gt frame.
         rescells (np arr): Array of overlapping ids in the res frame.
     """
-    if len(gt.shape) != 2 or len(res.shape) != 2:
-        raise ValueError("gt and res must be 2d arrays")
-
     iou = np.zeros((np.max(gt) + 1, np.max(res) + 1))
 
     overlapping_gt_labels, overlapping_res_labels = get_labels_with_overlap(gt, res)
@@ -46,20 +45,17 @@ def _match_nodes(gt, res, threshold=1):
     return gtcells, rescells
 
 
-def match_iou_2d(gt, pred, threshold=0.6, label_key="segmentation_id"):
+def match_iou_2d(gt, pred, threshold=0.6):
     """Identifies pairs of cells between gt and pred that have iou > threshold
 
     This can return more than one match for any node
     Assumes that within a frame, each object has a unique segmentation label
     and that the label is recorded on each node using label_key
-    Currently only supports 2d+t
 
     Args:
         gt (TrackingData): Tracking data object containing graph and segmentations
         pred (TrackingData): Tracking data object containing graph and segmentations
         threshold (float, optional): Minimum IoU for matching cells. Defaults to 0.6.
-        label_key (str, optional): Key for the segmentation label attribute on each node.
-            Defaults to "segmentation_id"
 
     Returns:
         list[(gt_node, pred_node)]: list of tuples where each tuple contains a gt node and pred node
@@ -85,21 +81,37 @@ def match_iou_2d(gt, pred, threshold=0.6, label_key="segmentation_id"):
     for i, t in enumerate(
         range(gt.tracking_graph.start_frame, gt.tracking_graph.end_frame)
     ):
-        matches = _match_nodes(mask_gt[i], mask_pred[i])
+        matches = _match_nodes(mask_gt[i], mask_pred[i], threshold=threshold)
 
         # Construct node id tuple for each match
         for gt_id, pred_id in zip(*matches):
             # Find node id based on time and segmentation label
             gt_node = G_gt.get_nodes_with_attribute(
-                label_key,
+                G_gt.label_key,
                 criterion=lambda x: x == gt_id,  # noqa
                 limit_to=G_gt.get_nodes_in_frame(t),
             )[0]
             pred_node = G_pred.get_nodes_with_attribute(
-                label_key,
+                G_pred.label_key,
                 criterion=lambda x: x == pred_id,  # noqa
                 limit_to=G_pred.get_nodes_in_frame(t),
             )[0]
             mapper.append((gt_node, pred_node))
 
     return mapper
+
+
+class IOUMatched(Matched):
+    def __init__(self, gt_data, pred_data, iou_threshold=0.6):
+        super().__init__(gt_data, pred_data)
+
+        self.iou_threshold = iou_threshold
+
+        # Check that segmentations exist in the data
+        if gt_data.segmentation is None or pred_data.segmentation is None:
+            raise ValueError(
+                "Segmentation data must be provided for both gt and pred data"
+            )
+
+    def compute_mapping(self):
+        return match_iou_2d(self.gt_data, self.pred_data, threshold=self.iou_threshold)
