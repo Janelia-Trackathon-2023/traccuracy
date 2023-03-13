33b291c8d5e49533f8e84610cc178347ca44a640
msschwartz21 <msschwartz21@gmail.com>
2023-03-13 15:13:42 -0400

    add docstring'

--- src/cell_tracking_metrics/matchers/matched.py
+++ src/cell_tracking_metrics/matchers/matched.py
@@ -5,18 +5,28 @@ from ..tracking_data import TrackingData
 
 class Matched(ABC):
     def __init__(self, gt_data: "TrackingData", pred_data: "TrackingData"):
+        """Matched class which takes TrackingData objects for gt and pred, and computes matching.
+
+        Each current matching method will be a subclass of Matched e.g. CTCMatched or IOUMatched.
+        The Matched objects will store both gt and pred data, as well as the mapping,
+        and any additional private attributes that may be needed/used e.g. detection matrices.
+
+        Args:
+            gt_data (TrackingData): Tracking data object for the gt
+            pred_data (TrackingData): Tracking data object for the pred
+        """
         self.gt_data = gt_data
         self.pred_data = pred_data
 
-        self._mapping = None
+        self.mapping = self.compute_mapping()
 
     @abstractmethod
     def compute_mapping(self):
-        raise NotImplementedError
+        """Computes a mapping of nodes in gt to nodes in pred
 
-    @property
-    @abstractmethod
-    def mapping(self):
-        if self._mapping is None:
-            self.compute_mapping()
-        return self._mapping
+        The mapping must be a list of tuples, e.g. [(gt_node, pred_node)]
+
+        Raises:
+            NotImplementedError
+        """
+        raise NotImplementedError
--- src/cell_tracking_metrics/metrics/base.py
+++ src/cell_tracking_metrics/metrics/base.py
@@ -12,8 +12,28 @@ class Metric(ABC):
     supports_many_to_many = False
 
     def __init__(self, matched_data: "Matched"):
+        """Add Matched class which takes TrackingData objects for gt and pred,and computes matching
+
+        Each current matching method will be a subclass of Matched e.g. CTCMatched or IOUMatched.
+        The Matched objects will store both gt and pred data, as well as the mapping,
+        and any additional private attributes that may be needed/used e.g. detection matrices.
+        Metric subclasses will take keyword arguments to set the weights of various error counts.
+
+        Args:
+            matched_data (Matched): Matched object for set of GT and Pred data
+        """
         self.data = matched_data
 
     @abstractmethod
     def compute(self) -> dict:
+        """The compute methods of Metric objects return a dictionary with counts and statistics.
+
+        They may make use of TrackingEvents objects but do not have to.
+
+        Raises:
+            NotImplementedError
+
+        Returns:
+            dict: Dictionary of metric names and int/float values
+        """
         raise NotImplementedError
