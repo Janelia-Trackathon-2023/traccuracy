434401468e41fe5c480badfcfea7eaaebe8b1d7b
msschwartz21 <msschwartz21@gmail.com>
2023-09-08 12:14:46 -0700

    Fix loaders

--- src/traccuracy/loaders/__init__.py
+++ src/traccuracy/loaders/__init__.py
@@ -1,8 +1,8 @@
 """ Subpackage for loading tracking data into memory
 
 This subpackage contains functions for loading ground
-truth or tracking method outputs into memory as TrackingData objects.
-Each loading function must return one TrackingData object which has a
+truth or tracking method outputs into memory as TrackingGraph objects.
+Each loading function must return one TrackingGraph object which has a
 track graph and optionally contains a corresponding segmentation.
 """
 from ._ctc import load_ctc_data
