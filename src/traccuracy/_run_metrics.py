0c809f0bf768f9e65463ece1222313553e3f039d
msschwartz21 <msschwartz21@gmail.com>
2023-11-14 16:46:50 -0800

    Refactor run_metrics to use instantiated objects for matchers and metrics'

--- src/traccuracy/_run_metrics.py
+++ src/traccuracy/_run_metrics.py
@@ -1,9 +1,7 @@
 from typing import TYPE_CHECKING
 
-from traccuracy._utils import get_relevant_kwargs
-
 if TYPE_CHECKING:
-    from typing import Dict, List, Optional, Type
+    from typing import Dict, List
 
     from traccuracy import TrackingGraph
     from traccuracy.matchers._base import Matcher
@@ -13,42 +11,28 @@ if TYPE_CHECKING:
 def run_metrics(
     gt_data: "TrackingGraph",
     pred_data: "TrackingGraph",
-    matcher: "Type[Matcher]",
-    metrics: "List[Type[Metric]]",
-    matcher_kwargs: "Optional[Dict]" = None,
-    metrics_kwargs: "Optional[Dict]" = None,  # weights
+    matcher: "Matcher",
+    metrics: "List[Metric]",
 ) -> "Dict":
     """Compute given metrics on data using the given matcher.
 
-    An error will be thrown if the given matcher is not compatible with
-    all metrics in the given list. The returned result dictionary will
-    contain all metrics computed by the given Metric classes, as well as
-    general summary numbers e.g. false positive/false negative detection
-    and edge counts.
+    The returned result dictionary will contain all metrics computed by
+    the given Metric classes, as well as general summary numbers
+    e.g. false positive/false negative detection and edge counts.
 
     Args:
-        gt_data (TrackingData): ground truth graph and optionally segmentation
-        pred_data (TrackingData): predicted graph and optionally segmentation
-        matcher (traccuracy.matchers.Matched): matching class to use to create correspondence
-        metrics (List[Metric]): list of metrics to compute as class names
-        matcher_kwargs (optional, dict): Dictionary of keyword argument for the
-            matcher class
-        metric_kwargs (optional, dict): Dictionary of any keyword args for the
-            Metric classes
+        gt_data (TrackingGraph): ground truth graph and optionally segmentation
+        pred_data (TrackingGraph): predicted graph and optionally segmentation
+        matcher (traccuracy.matchers.Matcher): instantiated matcher object
+        metrics (List[Metric]): list of instantiated metrics objects to compute
 
     Returns:
         Dict: dictionary of metrics indexed by metric name. Dictionary will be
-        nested for metrics that return multiple values.
+            nested for metrics that return multiple values.
     """
-    if matcher_kwargs is None:
-        matcher_kwargs = {}
-    matched = matcher(**matcher_kwargs).compute_mapping(gt_data, pred_data)
-    metric_kwarg_dict = {
-        m_class: get_relevant_kwargs(m_class, metrics_kwargs) for m_class in metrics
-    }
+    matched = matcher.compute_mapping(gt_data, pred_data)
     results = {}
     for _metric in metrics:
-        relevant_kwargs = metric_kwarg_dict[_metric]
-        result = _metric(**relevant_kwargs).compute(matched)
-        results[_metric.__name__] = result
+        result = _metric.compute(matched)
+        results[_metric.__class__.__name__] = result
     return results
