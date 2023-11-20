3aea4c149ffe16cb91d81e3983f5624a4a651f15
Talley Lambert <talley.lambert@gmail.com>
2023-11-20 15:24:13 -0500

    fix import loop

--- src/traccuracy/__init__.py
+++ src/traccuracy/__init__.py
@@ -6,9 +6,7 @@ try:
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "uninstalled"
 
+from ._run_metrics import run_metrics
 from ._tracking_graph import EdgeAttr, NodeAttr, TrackingGraph
-
-# must go after TrackingGraph to avoid circular imports
-from ._run_metrics import run_metrics  # isort:skip
 
 __all__ = ["TrackingGraph", "run_metrics", "NodeAttr", "EdgeAttr"]
