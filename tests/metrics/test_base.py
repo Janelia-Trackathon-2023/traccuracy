8727d0c64d2103f69d4777508155f1bdb54172ec
Morgan Schwartz <msschwartz21@gmail.com>
2025-02-05 17:01:25 -0500

    Add test for matcher override

--- tests/metrics/test_base.py
+++ tests/metrics/test_base.py
@@ -28,7 +28,7 @@ class TestMetric:
     def test_empty_list(self):
         class DummyMetric(Metric):
             def __init__(self):
-                super().__init__([])
+                super().__init__(valid_matches=[])
 
             def _compute(self):
                 pass
@@ -43,10 +43,42 @@ class TestMetric:
 
         class DummyMetric(Metric):
             def __init__(self):
-                super().__init__([bad_option])
+                super().__init__(valid_matches=[bad_option])
 
             def _compute(self):
                 pass
 
         with pytest.raises(ValueError, match=r"Matching type .* is not supported."):
             DummyMetric()
+
+    def test_matcher_override(self):
+        class DummyMetric(Metric):
+            def __init__(self):
+                super().__init__(valid_matches=["one-to-one"])
+
+            def _compute(self):
+                return {"success": True}
+
+        graph = TrackingGraph(nx.DiGraph())
+        matched = Matched(
+            graph, graph, [(1, 2), (1, 3)], {"matching type": "many-to-many"}
+        )
+
+        metric = DummyMetric()
+
+        # Fail without override
+        message = (
+            "The matched data uses a matcher that does not meet the requirements "
+            "of the metric. Check the documentation for the metric for more information."
+        )
+        with pytest.raises(TypeError, match=message):
+            metric.compute(matched)
+
+        # Override triggers warning
+        message = (
+            "Overriding matcher/metric validation may result in "
+            "unpredictable/incorrect metric results"
+        )
+        with pytest.raises(UserWarning, match=message):
+            results = metric.compute(matched, override_matcher=True)
+            assert "success" in results.results
