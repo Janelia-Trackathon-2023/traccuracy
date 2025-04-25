c883707e81f1e1d36d4dd7882e2776a964434a0f
Morgan Schwartz <msschwartz21@gmail.com>
2025-04-25 11:56:28 -0400

    Load csv into TrackingGraph

---
+++ src/traccuracy/loaders/_point.py
@@ -0,0 +1,90 @@
+from typing import cast
+
+import networkx as nx
+import pandas as pd
+
+from traccuracy._tracking_graph import TrackingGraph
+
+
+def load_point_data(
+    path: str | None = None,
+    df: pd.DataFrame | None = None,
+    parent_column: str = "parent",
+    id_column: str | None = None,
+    pos_columns: tuple[str, ...] = ("z", "y", "x"),
+    time_column: str = "t",
+    name: str | None = None,
+    sep: str | None = None,
+) -> TrackingGraph:
+    """Load point-based tracking data into a TrackingGraph from a csv-like file
+
+    Assumes each row contains:
+    - time
+    - position, e.g. three columns 'z', 'y', 'x'
+    - parent, a reference to the node in the previous time frame
+
+    Args:
+        path (str | None, optional): Path to the csv-like file to load. Defaults to None.
+        df (pd.DataFrame | None, optional): A dataframe that has already been loaded.
+            Defaults to None.
+        parent_column (str | None, optional): A reference to the parent node in the previous
+            time frame. Defaults to "parent".
+        id_column (str | None, optional): Optional column used to specify node ids.
+            Defaults to None and the row index is used as the id
+        pos_columns (tuple[str], optional): A tuple of columns to use for position.
+            Defaults to ("z", "y", "x").
+        time_column (str, optional): The column to use for time. Defaults to "t".
+        name (str | None, optional): Optional string to name/describe the dataset. Defaults to None.
+        sep (str | None, optional): Passed to pd.read_csv to set the sep kwarg. Defaults to None.
+
+    Raises:
+        ValueError: Must provide either a path or a dataframe
+        ValueError: parent_column not present in data
+        ValueError: id_column not present in data
+        ValueError: pos_columns not present in data
+        ValueError: time_column not present in data
+
+    Returns:
+        TrackingGraph
+    """
+    if path is None and df is None:
+        raise ValueError("Must provide either a path or a dataframe")
+
+    if path:
+        if sep is None:
+            df = pd.read_csv(path)
+        else:
+            df = pd.read_csv(path, sep=sep)
+
+    # At this point, df is guaranteed to be dataframe not None
+    df = cast("pd.DataFrame", df)
+
+    if parent_column not in df.columns:
+        raise ValueError(f"Specified parent_column {parent_column} not present")
+
+    if id_column and id_column not in df.columns:
+        raise ValueError(f"Specified id_column {id_column} not present")
+
+    if not all(c in df.columns for c in pos_columns):
+        raise ValueError(f"Specified pos_columns {pos_columns} not present")
+
+    if time_column not in df.columns:
+        raise ValueError(f"Specified time_column {time_column} not present")
+
+    nodes, edges = [], []
+    for idx, row in df.iterrows():
+        if id_column is None:
+            node_id = idx
+        else:
+            node_id = row[id_column]
+
+        nodes.append((node_id, row[[time_column, *pos_columns]].to_dict()))
+
+        if row[parent_column] != -1:
+            edges.append((row[parent_column], node_id))
+
+    G: nx.DiGraph = nx.DiGraph()
+    G.add_nodes_from(nodes)
+    G.add_edges_from(edges)
+
+    return TrackingGraph(G, frame_key=time_column, location_keys=pos_columns, name=name)
---
+++ tests/loaders/test_point.py
@@ -0,0 +1,96 @@
+import os
+
+import pandas as pd
+import pytest
+
+from traccuracy.loaders._point import load_point_data
+
+
+class Test_load_point_data:
+    def get_valid_df(self, nrows):
+        df = pd.DataFrame(
+            {
+                "parent": range(-1, nrows - 1),
+                "t": range(nrows),
+                "x": range(nrows),
+                "y": range(nrows),
+                "z": range(nrows),
+            }
+        )
+        return df
+
+    def test_exceptions(self):
+        nrows = 5
+
+        # No data source
+        with pytest.raises(ValueError, match="Must provide either a path or a dataframe"):
+            load_point_data()
+
+        # Bad parent column
+        data = {}
+        with pytest.raises(ValueError, match="Specified parent_column *"):
+            load_point_data(df=pd.DataFrame(data))
+
+        # Bad id column
+        data = {"parent": range(nrows)}
+        with pytest.raises(ValueError, match="Specified id_column *"):
+            load_point_data(df=pd.DataFrame(data), id_column="node_id")
+
+        # All pos columns missing
+        with pytest.raises(ValueError, match="Specified pos_columns *"):
+            load_point_data(df=pd.DataFrame(data))
+
+        # One pos column missing
+        data = {**data, "x": range(nrows), "y": range(nrows)}
+        with pytest.raises(ValueError, match="Specified pos_columns *"):
+            load_point_data(df=pd.DataFrame(data))
+
+        # Time missing
+        data = {**data, "z": range(nrows)}
+        with pytest.raises(ValueError, match="Specified time_column *"):
+            load_point_data(df=pd.DataFrame(data))
+
+    def test_load_from_dataframe(self):
+        # Make a valid dataframe using defaults
+        nrows = 5
+        df = self.get_valid_df(nrows)
+
+        track_graph = load_point_data(df=df, name="test")
+        assert len(track_graph.graph.nodes) == nrows
+        assert len(track_graph.graph.edges) == nrows - 1
+        assert track_graph.get_location(0) == [0, 0, 0]
+        assert track_graph.name == "test"
+
+        # Change parent default
+        df_mod = df.rename(columns={"parent": "pid"})
+        track_graph = load_point_data(df=df_mod, parent_column="pid")
+        assert len(track_graph.graph.edges) == nrows - 1
+
+        # Change pos default
+        df_mod = df.drop(columns=["z"])
+        track_graph = load_point_data(df=df_mod, pos_columns=("y", "x"))
+        assert track_graph.get_location(0) == [0, 0]
+
+        # Change time default
+        df_mod = df.rename(columns={"t": "time"})
+        track_graph = load_point_data(df=df_mod, time_column="time")
+        assert track_graph.frame_key == "time"
+        assert "time" in track_graph.graph.nodes[0]
+
+        # Change id default
+        df["node_id"] = range(10, 10 + nrows)
+        df["parent"] = [-1, *list(range(10, 10 + nrows - 1))]
+        track_graph = load_point_data(df=df, id_column="node_id")
+        assert set(track_graph.nodes.keys()) == set(df["node_id"])
+
+    def test_csv_kwargs(self, tmpdir):
+        df = self.get_valid_df(5)
+        filepath = os.path.join(tmpdir, "test.csv")
+
+        df.to_csv(filepath)
+        load_point_data(filepath)
+
+        # Test different separator
+        sep = "\t"
+        df.to_csv(filepath, sep=sep)
+        load_point_data(filepath, sep=sep)
